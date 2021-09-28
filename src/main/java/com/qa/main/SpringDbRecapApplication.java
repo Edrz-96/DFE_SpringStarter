da63b64d7e481ba3a65d2c34674fa23575a66781
Edrz-96 <edrz94@gmail.com>
2021-09-28 10:31:09 +0100

    first upload

---
+++ src/main/java/com/qa/main/SpringDbRecapApplication.java
@@ -0,0 +1,16 @@
+package com.qa.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.qa.main.domain.Student;
+
+@SpringBootApplication
+public class SpringDbRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbRecapApplication.class, args);
+		
+	}
+
+}
